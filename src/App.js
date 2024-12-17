cc779b40730114ac54aff5a39eb38df360f45a6a
amish thapa <thapaamish805@gmail.com>
2024-12-17 18:12:58 +0545

    check

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React AMshjD
+          Learn React Amish
         </a>
       </header>
     </div>
